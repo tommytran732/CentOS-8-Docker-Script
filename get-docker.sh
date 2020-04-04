82a46470970f3489c7de4dc0e88512f1b5aaa033
TommyTran732 <57488583+tommytran732@users.noreply.github.com>
2020-04-04 03:38:53 -0400

    Update get-docker.sh

--- get-docker.sh
+++ get-docker.sh
@@ -5,7 +5,7 @@ output(){
 }
 
 preflight(){
-    output "CentOS 8 Docker Installation Script"
+    output "RHEL/CentOS 8 Docker Installation Script"
     output "Copyright © 2020 Thien Tran <contact@thientran.io>."
     output "Support: https://thientran.io/discord"
     output ""
